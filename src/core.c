df85d20a9ca29b3c751fceaa5a6aa36bb79a8f73
HIROSHI OOTA <flied.beens@gmail.com>
2015-07-09 14:22:04 +0900

    Remove dll directory setup. Gauche loads dll with full path.

--- src/core.c
+++ src/core.c
@@ -671,7 +671,7 @@ BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason,LPVOID lpvReserved)
 
     switch(fdwReason) {
     case DLL_PROCESS_ATTACH:
-	setup_dll_directory(hinstDLL);
+//	setup_dll_directory(hinstDLL);
 	break;
     case DLL_PROCESS_DETACH:
 	break;
